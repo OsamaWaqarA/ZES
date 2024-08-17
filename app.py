dd7794ac78272c6d8647e2ad144eccf75d019b2d
OSamaWaqarA <oswaqar@gmail.com>
2024-08-17 14:57:42 +0500

    osama

--- app.py
+++ app.py
@@ -1,10 +1,37 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/update",methods=['GET','POST'])
+def update():
+    data = request.get_json()
+    want = data.get("want")
+    if want == "all":
+        return jsonify({'mute' : "True",'time' : 6,'volume': 50, 'pause' : "True", "full": "True"}) 
+
+@app.route("/command",methods=['GET','POST'])
+def command():
+    data = request.get_json()
+    mute = data.get("mute")
+    timeing = data.get("time")
+    volume = data.get("volume")
+    pause = data.get("pause")
+    full = data.get("full")
+
+    with open("command.txt","r")as file:
+        file .write(pause)
+        file.write("/n")
+        file.write(mute)
+        file.write("/n")
+        file.write(timeing)
+        file.write("/n")
+        file.write(volume)
+        file.write("/n")
+        file.write(full)
 
 if __name__ == "__main__":
     app.run(host = "0.0.0.0",port = 8000,debug = True)
--- static/webdata/js/vid.js
+++ static/webdata/js/vid.js
@@ -1,16 +1,47 @@
 playbtn = document.getElementById("play");
 mutebtn = document.getElementById("mute");
 fullbtn = document.getElementById("full");
-playbtn.addEventListener("click",playpause,false);
-mutebtn.addEventListener("click",muteunmute,false);
-fullbtn.addEventListener("click",fullnofull,false);
+seek = document.getElementById("seek");
+volume = document.getElementById("volume");
+playbtn.addEventListener("click",sentdata,false);
+mutebtn.addEventListener("click",sentdata,false);
+fullbtn.addEventListener("click",sentdata,false);
+seek.addEventListener("change",seek_move,false);
+volume.addEventListener("click",sentdata,false);
 
-function playpause(){
-    playbtn.style.backgroundImage = "url(/static/webdata/images/pause.png)";
+function sentdata(){
+    update();  
 }
-function muteunmute(){
-
-}
-function fullnofull(){
+function seek_move(){
     
+}
+
+function update(){
+    fetch("update",{
+        method:'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({want:"all"})
+    })
+    .then(res => res.json())
+    .then(data =>{
+        seek.value = data.time;
+        volume.value = data.volume;
+        if (data.pause == "True"){
+            playbtn.style.backgroundImage = "url(/static/webdata/images/pause.png)";
+        }else{
+            playbtn.style.backgroundImage = "url(/static/webdata/images/play.png)";
+        }
+        if (data.mute == "True"){
+            mutebtn.style.backgroundImage = "url(/static/webdata/images/unmute.png)";
+        }else{
+            mutebtn.style.backgroundImage = "url(/static/webdata/images/mute.png)"
+        }
+        if (data.full == "True"){
+            fullbtn.style.backgroundImage = "url(/static/webdata/images/fullscreen.png)"
+        }else{
+            //add the other pic here
+        }
+    }) 
 }
